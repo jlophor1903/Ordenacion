4fd51605a265d2a3a9c0fc8e7ab5f9b737ea22f7
jlophor1903 <jlophor1903@g.educaand.es>
2025-04-01 10:06:33 +0200

    treselementos

--- treselementos.py
+++ treselementos.py
@@ -1,11 +1,9 @@
 n = [65, 23 ,42]
 
 # Comparamos los valores y los ordenamos
-if n[0] > n[1]:
-    n[0], n[1] = n[1], n[0]
-if n[1] > n[2]:
-    n[1], n[2] = n[2], n[1]
-if n[0] > n[1]:
-    n[0], n[1] = n[1], n[0]
+for i in range(2)
+if n[i] > n[i+1]:
+    n[i], n[i+1] = n[i+1], n[i]
+
 
 print(n)
