96293c5f73a54539fa4ad8e8df8e5d952cc9bf4b
jlophor1903 <jlophor1903@g.educaand.es>
2025-05-13 09:55:53 +0200

    widgets nuevo

--- treselementos.py
+++ treselementos.py
@@ -1,4 +1,15 @@
+#Archivo: treselementos.py
+#Fecha: 08/04/2025
+#proy.: estudio de algoritmos
+#
+# Ordenar crecientemente una lista de tres valores
+# Existe un error, ya que según los valores, puede cambiar
+# dos que entre ellos había que cambiar, perogenera mal orden 
+# en los posteriores. 
+
 n = [252 , 14, 58]
+
+# Bucle para tomar datos del teclado y guardarlos en la variable n de tipo lista
 def swap(j):
     a = n[j]
     n[j] = n[j+1]
